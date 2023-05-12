54dafc33777b4aab519cc30572a5f728ae415674
Kaurkaur123 <131433724+Kaurkaur123@users.noreply.github.com>
2023-05-12 09:31:24 +0300

    pool

--- foorum/polls/views.py
+++ foorum/polls/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.template import loader
+from django.views import generic
+from .models import Question, Response
+from django.http import HttpResponse
+
+class IndexView(generic.Listview):
+    template = loader.get_template('polls/index.html')
+    kusimus = Question.objects.all().values()
+    context = {
+        'kusimus': kusimus,
+    }
+
 
 # Create your views here.
