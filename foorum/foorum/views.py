8d7845a1481aa61920034811e499ba8f419eee8d
OkiEmil <roosteemil@gmail.com>
2023-05-19 09:09:48 +0300

    kk

--- foorum/foorum/views.py
+++ foorum/foorum/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 from polls.models import Question
 # Create your views here.
 
-#def signup (request):
+def signup (request):
 #	questions = Question.objects.all().order_by("-created_at")
 #	context = {
 #		"questions" : questions
 #	}
-#	return render(request,'Kasutajad/signup.html', context)
+	return render(request,'Kasutajad/signup.html')
