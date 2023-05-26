e64afea1e2ac06fe10ac5a453e800691cf7d86b2
Kaurkaur123 <131433724+Kaurkaur123@users.noreply.github.com>
2023-05-26 08:09:05 +0300

    Revert "Revert "Merge branch 'main' of https://github.com/Kaurkaur123/Django-question-forum-project""
    This reverts commit de719c29219c29077d9b6f8e4da273c53731352f.

--- foorum/polls/views.py
+++ foorum/polls/views.py
@@ -5,13 +5,13 @@ from django.views import generic
 from .models import Question, Response
 from django.http import HttpResponse
 
-def IndexView(request):
-    template = loader.get_template('polls/index.html')
-    questions = Question.objects.all().values()
-    context = {
-        'questions': questions,
-    }
-    return HttpResponse(template.render(context, request))
+#def IndexView(request):
+#    template = loader.get_template('polls/index.html')
+#    questions = Question.objects.all().values()
+#    context = {
+#        'questions': questions,
+#    }
+#    return HttpResponse(template.render(context, request))
 
 
 # Create your views here.
