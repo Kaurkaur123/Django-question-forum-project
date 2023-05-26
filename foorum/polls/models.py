0120f50023cb19765be0c05d90c596583c1feb82
Kaurkaur123 <131433724+Kaurkaur123@users.noreply.github.com>
2023-05-26 08:47:52 +0300

    t

--- foorum/polls/models.py
+++ foorum/polls/models.py
@@ -28,5 +28,20 @@ class Response(models.Model):
     def __str__(self):
         return self.body
 
+class Comment(models.Model):
+    #vaheasin Post Questioniks
+    post = models.ForeignKey(Question,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
+
 #    def get_responses(self):
 #        return Response.objects.filter(parent=self)
--- foorum/polls/urls.py
+++ foorum/polls/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
+    path('<slug:slug>/', views.post_detail, name='post_detail')
     #path('', views.TabelView.as_view(), name='tabel')
     #path('', views.IndexView, name='polls')
 ]
